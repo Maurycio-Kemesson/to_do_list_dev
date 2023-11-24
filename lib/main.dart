bcb76686dc3ade2e60ab60e9dad6e550cb43b178
MaurycioKemesson <maurycio.kemesson@wdatecnologia.com.br>
2023-11-24 16:02:43 -0300

    fix: debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         appBarTheme: const AppBarTheme(
